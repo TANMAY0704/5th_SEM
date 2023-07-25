02f1a62aea587f909358a374912b26fcdfd6f555
Tanmay Pandey <PANDEY.TANMAY17@GMAIL.COM>
2023-07-25 20:50:04 +0530

    hi

--- CN/LAB1/1.c
+++ CN/LAB1/1.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct swap{
+    int a;
+    int b;
+};
 
+void swap(struct swap *s)
+{
+    int temp = s->a;
+    s->a = s->b;
+    s->b = temp;
+}
 
 int main()
 {
+    struct swap s;
+    scanf("%d %d", &s.a, &s.b);
+    swap(&s);
+    printf("%d %d", s.a, s.b);
+    
     return 0;
 }
