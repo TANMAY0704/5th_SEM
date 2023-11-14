9dccdf0d1aacfc0902663b5097d34da99f5a2c70
Tanmay Pandey <PANDEY.TANMAY17@GMAIL.COM>
2023-11-14 23:10:17 +0530

    SE

--- CN/PRACTISE/client.c
+++ CN/PRACTISE/client.c
@@ -4,66 +4,21 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <server_ip> <server_port>\n", argv[0]);
-        exit(1);
-    }
+int main() {
+    int s_fd,c_fd;
+    struct sockaddr_in s_addr,c_addr;
+    int addrlen = sizeof(c_addr);
 
-    char *server_ip = argv[1];
-    int server_port = atoi(argv[2]);
-
-    // Create socket
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
+    if ((c_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket creation failed");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    // Connect to the server
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(server_port);
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (inet_pton(AF_INET, server_ip, &server_address.sin_addr) <= 0) {
-        perror("Invalid IP address");
-        close(client_socket);
-        exit(1);
-    }
-
-    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
-        perror("Connection failed");
-        close(client_socket);
-        exit(1);
-    }
-
-    printf("Connected to server %s:%d\n", server_ip, server_port);
-
-    // Exchange messages
-    char buffer[256];
-    int bytes_received;
-
-    do {
-        printf("Enter a message to send (type 'close' to end the connection): ");
-        fgets(buffer, sizeof(buffer), stdin);
-        buffer[strcspn(buffer, "\n")] = '\0'; // Remove trailing newline
-
-        send(client_socket, buffer, strlen(buffer), 0);
-
-        bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
-        if (bytes_received == -1) {
-            perror("Receive error");
-            break;
-        }
-
-        buffer[bytes_received] = '\0';
-        printf("Received response from server: %s\n", buffer);
-
-    } while (strcmp(buffer, "close") != 0);
-
-    // Close the socket
-    close(client_socket);
+    
+    
 
     return 0;
 }
